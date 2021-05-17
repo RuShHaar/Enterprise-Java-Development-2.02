4b034edfa8d870f7fc856d6dda4da4969ed6abc7
RuShHaar <stefan.ockert@gmx.net>
2021-05-17 23:15:31 +0200

    Aufgabe 2 erledigt // Unnötige Kommentare gelöscht

--- src/test/java/Task2/ForbiddenStringsTest.java
+++ src/test/java/Task2/ForbiddenStringsTest.java
@@ -9,13 +9,7 @@ class ForbiddenStringsTest {
     @Test
     void isForbidden() {
 
-        /* String[] keywords = {"abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package",
-                "synchronized", "boolean", "do", "if", "private", "this", "break",
-                "double", "implements", "protected", "throw", "byte", "else", "import", "public",
-                "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends",
-                "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally",
-                "long", "strictfp", "volatile", "const", "float", "native", "super", "while"};
-        */
+
         ForbiddenStrings test1 = new ForbiddenStrings();
 
         String sentence1 = "Ist die Welt nicht schoen";
